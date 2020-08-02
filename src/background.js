bc09a87aeda80501f9de14117bbc56b690521324
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-08-02 04:43:57 -0400

    add comments on the js files

--- src/background.js
+++ src/background.js
@@ -1,13 +1,21 @@
+/**
+ * The code in this file works to inject "default mode" code to the background webpage as soon as it is loaded so that the mode can be used on refreshes or from the get go
+ */
+
+// When the background webpage is updated
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  // check if the url is a chrome internal url
   if (!tab.url || !tab.url.startsWith('http')) {
     return;
   }
 
+  // code to inject
   const code = `document.addEventListener("keydown",(event) => {
           if(event.ctrlKey && event.key === "\`") {
               debugger;
           }
       });`;
+  //   injecting the code in the current webpage
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.executeScript(tabId, {
       code: code,
--- src/popup.js
+++ src/popup.js
@@ -1,28 +1,51 @@
+/**
+ * The code in this file contains logic to get use input on the custom settings, and injects the "debugger" code into the background when the use input is recieved
+ */
+
+/**
+ * This function opens a separate tab with information about "debugger" keyword on w3schools
+ * @param e event triggered on the href element click
+ * @returns void
+ */
 const visitInfoPage = (e) => {
   e.preventDefault();
   const url = `https://www.w3schools.com/jsref/jsref_debugger.asp`;
   chrome.tabs.create({ url: url });
 };
 
+/**
+ * This function adds event listeners to the necessary elements as soon as the popup is loaded
+ * @returns void
+ */
 const initial = () => {
   const submitButton = document.getElementById('submit-btn');
   const infoLink = document.getElementById('info');
   infoLink.addEventListener('click', visitInfoPage);
+  submitButton.addEventListener('click', handleSearchClick);
   document.addEventListener('keypress', (e) => {
+    //if "Enter" key is simulate click on the submit button
     if (e.key === 'Enter') {
       submitButton.click();
     }
   });
-  submitButton.addEventListener('click', handleSearchClick);
 };
 
+/**
+ * This function injects "debugger" keyword along with the keydown event listeners with the designated timeout
+ * @param  key key character the use wants to use in the combo
+ * @param  timeout number seconds of timeout
+ * @returns void
+ */
 const sendScript = (key, timeout) => {
+  // if empty
   if (key === '' || key === ' ') return;
+  // code to inject
   const code = `document.addEventListener("keydown",(event) => {
                   if(event.ctrlKey && event.key === "${key}") {
                       setTimeout(function(){debugger;},${timeout * 1000});
                   }
               });`;
+  // injecting the dynamic code generated
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.executeScript(tabs[0].id, {
       code: code,
@@ -30,12 +53,19 @@ const sendScript = (key, timeout) => {
   });
 };
 
+/**
+ * This function handles the clicking of the "Click here" button in the popup
+ * @param e event triggered when the button is clicked
+ * @returns void
+ */
 const handleSearchClick = (e) => {
   e.preventDefault();
   const errorElement = document.getElementById('error');
   const key = document.getElementById('key-macro').value;
   const timeout = Number(document.getElementById('timeout').value);
+  // reset error message text
   errorElement.innerHTML = '';
+  // if the key length is more than 1, set error message text
   if (key.length > 1) {
     errorElement.innerHTML = "The input can't be more than one character 😅";
   } else {
@@ -43,4 +73,5 @@ const handleSearchClick = (e) => {
   }
 };
 
+// when the popup is loaded
 document.addEventListener('DOMContentLoaded', initial);
